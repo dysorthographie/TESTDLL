b6ee6722dcc6f6dd7e3b276ef8e41400a5cec4bd
DURUPT <durupt.robert@hotmail.fr>
2022-12-17 20:27:23 +0100

    Ajoutez des fichiers projet.

---
+++ TESTDLL/TESTDLL.vb
@@ -0,0 +1,8 @@
+﻿Public Class TESTDLL
+    Public ReadOnly Property Formulaire1() As Form1
+        Get
+            Formulaire1 = New Form1
+        End Get
+    End Property
+
+End Class
---
+++ TesterMyDll/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Dim MyDll As TESTDLL.TESTDLL
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim f As Object = MyDll.Formulaire1
+        f.show
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MyDll = New TESTDLL.TESTDLL
+    End Sub
+End Class
